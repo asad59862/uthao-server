26340b6a2b0f5da6f28aefa61234d648bde4d2d0
asad59862 <mda457956@gmail.com>
2025-08-30 16:41:43 +0600

    first commit

---
+++ convertKey.js
@@ -0,0 +1,9 @@
+// const fs = require('fs');
+// const key = fs.readFileSync('./firebase-admin-key.json', 'utf8');
+// const base64 = Buffer.from(key).toString('base64')
+// console.log(base64)
+
+
+// index .js use
+// const decodedKey = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf8');
+// const serviceAccount = JSON.parse(decodedKey);
